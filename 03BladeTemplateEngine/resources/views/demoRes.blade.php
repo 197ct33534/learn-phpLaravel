ce11161a9ec1fc2aad11adf1e13a589f3f5c1da9
197ct33534 <76165205+197ct33534@users.noreply.github.com>
2022-06-29 18:29:31 +0700

    doing respone

---
+++ 03BladeTemplateEngine/resources/views/demoRes.blade.php
@@ -0,0 +1,2 @@
+<h1>con diễm khùng</h1>
+{{ $title }}
--- 03BladeTemplateEngine/routes/web.php
+++ 03BladeTemplateEngine/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +21,27 @@ Route::get('/sanpham', [HomeController::class, 'product'])->name('product');
 Route::get('/add', [HomeController::class, 'getAdd'])->name('getadd');
 Route::post('/add', [HomeController::class, 'postAdd'])->name('postadd');
 Route::put('/add', [HomeController::class, 'putAdd'])->name('putadd');
+Route::get('/demo-response', function (){
+    // $content = json_encode([
+    //     'item1',
+    //     'item2',
+    //     'item3',
+    // ]);
+    // $res = (new Response($content,201))->header("Content-Type","application/json");
+    // $res = new Response();
+    // $res = response();
+
+    $res = (new Response())->cookie("devnghia","nghĩa đang học laravel với cookie 30 phút",30);
+    return $res;
+});
+
+Route::get('/demo-response-2', function (Request $request){
+    return $request->cookie("devnghia");
+});
+Route::get('/demo-response-3', function (){
+    $res = response()->view('demoRes',[
+        'title'=>"nghĩa nè"
+    ])->header('Content-Type', 'application/json')->header('API-key',123456);
+    return $res;
+
+});
