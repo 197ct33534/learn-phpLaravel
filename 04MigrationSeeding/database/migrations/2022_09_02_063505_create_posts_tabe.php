09d3d7207f5b2bc60686d0087ae1adc4057a80b7
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-09-02 13:44:35 +0700

    cách kiểm tra bảng hoặc cột trước khi thêm

---
+++ 04MigrationSeeding/database/migrations/2022_09_02_063505_create_posts_tabe.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('posts')) {
+            Schema::create('posts', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('title'); // default title 255
+                $table->text('content'); // default content not longText
+                $table->boolean('status'); // default tinyint status
+                $table->timestamp('pulished_at'); // default timestamp
+
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
