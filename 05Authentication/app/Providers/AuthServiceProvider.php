39391035b4348900171a3054a2098b17d83ade3f
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-09-08 00:35:48 +0700

    đăng ký policy

--- 05Authentication/app/Providers/AuthServiceProvider.php
+++ 05Authentication/app/Providers/AuthServiceProvider.php
@@ -18,7 +18,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Posts::class => PostPolicy::class
     ];
 
     /**
