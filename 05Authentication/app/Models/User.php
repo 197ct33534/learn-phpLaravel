eef521296c8639cf8cfe1d5994b639176ab85f84
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-09-08 00:57:44 +0700

    tạo phương thức is_superAdmin

--- 05Authentication/app/Models/User.php
+++ 05Authentication/app/Models/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function is_superAdmin()
+    {
+        if ($this->group_id == 2) {
+            return true;
+        }
+        return false;
+    }
 }
