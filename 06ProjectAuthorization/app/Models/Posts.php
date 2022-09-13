4fb9d037750a03e12ce19ad8115fb7e74b943a97
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-09-13 19:54:38 +0700

    create model posts

---
+++ 06ProjectAuthorization/app/Models/Posts.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Posts extends Model
+{
+    use HasFactory;
+    protected $table = 'posts';
+
+    public function getUser()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
