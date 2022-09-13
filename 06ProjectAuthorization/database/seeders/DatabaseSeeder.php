196f589098e20852e999e8b247bc9cfab32c2337
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-09-13 18:17:38 +0700

    fake data for table modules

--- 06ProjectAuthorization/database/seeders/DatabaseSeeder.php
+++ 06ProjectAuthorization/database/seeders/DatabaseSeeder.php
@@ -16,37 +16,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-        $groupID = DB::table('groups')->insertGetId([
-            'name' => 'Administrator',
-            'user_id' => 0,
+        // DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        // $groupID = DB::table('groups')->insertGetId([
+        //     'name' => 'Administrator',
+        //     'user_id' => 0,
+        //     'created_at' => date('Y-m-d H:i:s'),
+        //     'updated_at' => date('Y-m-d H:i:s'),
+
+        // ]);
+        // DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        // if ($groupID > 0) {
+        //     $userID = DB::table('users')->insertGetId([
+        //         'name' => 'Trung Nghĩa',
+        //         'email' => 'trantrungnghia07122001@gmail.com',
+        //         'password' => Hash::make(123456),
+        //         'group_id' => $groupID,
+        //         'user_id' => 0,
+        //         'created_at' => date('Y-m-d H:i:s'),
+        //         'updated_at' => date('Y-m-d H:i:s'),
+
+        //     ]);
+        //     if ($userID > 0) {
+        //         for ($i = 0; $i < 5; $i++) {
+        //             DB::table('posts')->insert([
+        //                 'title' => 'What is Lorem Ipsum? ' . $i,
+        //                 'content' => $i . 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum',
+        //                 'user_id' => $userID,
+        //                 'created_at' => date('Y-m-d H:i:s'),
+        //                 'updated_at' => date('Y-m-d H:i:s'),
+        //             ]);
+        //         }
+        //     }
+        // }
+
+        DB::table('modules')->insert([
+            'name' => 'users',
+            'title' => 'quản lí người dùng',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
-
         ]);
-        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
-        if ($groupID > 0) {
-            $userID = DB::table('users')->insertGetId([
-                'name' => 'Trung Nghĩa',
-                'email' => 'trantrungnghia07122001@gmail.com',
-                'password' => Hash::make(123456),
-                'group_id' => $groupID,
-                'user_id' => 0,
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
-
-            ]);
-            if ($userID > 0) {
-                for ($i = 0; $i < 5; $i++) {
-                    DB::table('posts')->insert([
-                        'title' => 'What is Lorem Ipsum? ' . $i,
-                        'content' => $i . 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum',
-                        'user_id' => $userID,
-                        'created_at' => date('Y-m-d H:i:s'),
-                        'updated_at' => date('Y-m-d H:i:s'),
-                    ]);
-                }
-            }
-        }
+        DB::table('modules')->insert([
+            'name' => 'groups',
+            'title' => 'quản lí nhóm',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('modules')->insert([
+            'name' => 'posts',
+            'title' => 'quản lí bài viết',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
     }
 }
