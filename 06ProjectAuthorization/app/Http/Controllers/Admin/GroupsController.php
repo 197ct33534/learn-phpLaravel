3d89c681d97ddd468b0b73447b4e2f69198edf6c
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-09-13 16:49:12 +0700

    add function edit , post edit , delete

--- 06ProjectAuthorization/app/Http/Controllers/Admin/GroupsController.php
+++ 06ProjectAuthorization/app/Http/Controllers/Admin/GroupsController.php
@@ -35,4 +35,32 @@ class GroupsController extends Controller
         $group->save();
         return redirect()->route('admin.groups.index')->with('msg', 'Thêm nhóm người dùng thành công');
     }
+
+    public function edit(Groups $group, Request $request)
+    {
+
+        return view('admin.groups.edit', compact('group'));
+    }
+    public function postEdit(Groups $group, Request $request)
+    {
+        $rules = ['name' => 'required|unique:groups,name,' . $group->id];
+        $messages = [
+            'name.required' => 'Tên nhóm không được để trống',
+            'name.unique' => 'Tên nhóm đã tồn tại',
+        ];
+        $request->validate($rules, $messages);
+        $group->name = $request->name;
+
+        $group->save();
+        return redirect()->route('admin.groups.index')->with('msg', 'Cập nhật nhóm người dùng thành công');
+    }
+    public function delete(Groups $group)
+    {
+        $numberUser = $group->getUser->count();
+        if ($numberUser) {
+            return redirect()->route('admin.groups.index')->with('msg', 'Trong nhóm vẫn còn ' . $numberUser . ' người dùng');
+        }
+        Groups::destroy($group->id);
+        return redirect()->route('admin.groups.index')->with('msg', 'Xóa nhóm người dùng thành công');
+    }
 }
