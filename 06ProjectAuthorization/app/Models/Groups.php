c3be97921e7d04024198c7897942a2d27e21c9ea
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-09-13 16:17:55 +0700

    add relation, get info user created group

--- 06ProjectAuthorization/app/Models/Groups.php
+++ 06ProjectAuthorization/app/Models/Groups.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Groups extends Model
 {
     use HasFactory;
     protected $table = 'groups';
+
+    // get info user created group
+    public function postBy()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
