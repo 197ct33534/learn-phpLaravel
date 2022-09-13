630098cc25b900c3c03554b9acba37cdfec0fd74
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-09-13 16:48:34 +0700

    create view edit groups

---
+++ 06ProjectAuthorization/resources/views/admin/groups/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.admin')
+
+@section('title', 'Cập nhật nhóm người dùng')
+@section('content')
+    @if ($errors->any())
+        <div class="text-center alert alert-danger"> Vui lòng kiểm tra dữ liệu nhập vào</div>
+    @endif
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Thêm nhóm người dùng</h1>
+    </div>
+    <form action="" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="nameGroup">Họ và tên</label>
+            <input name="name" type="text" class="form-control" id="nameGroup" value="{{ old('name') ?? $group->name }}">
+            @error('name')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror (session('name'))
+
+        </div>
+
+        <button type="submit" class="btn btn-primary">Cập nhật </button>
+    </form>
+@endsection('content')
