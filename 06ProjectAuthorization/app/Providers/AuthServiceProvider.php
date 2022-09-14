82f3084a5c366a9d74e675085931a90b9fbe6a82
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-09-14 11:59:11 +0700

    create policy , and define gate edit , delete of modules

--- 06ProjectAuthorization/app/Providers/AuthServiceProvider.php
+++ 06ProjectAuthorization/app/Providers/AuthServiceProvider.php
@@ -6,7 +6,8 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use App\Models\Modules;
 use App\Models\User;
-
+use App\Models\Posts;
+use App\Policies\PostsPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -17,6 +18,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Posts::class => PostsPolicy::class,
     ];
 
     /**
@@ -41,6 +43,29 @@ class AuthServiceProvider extends ServiceProvider
                     if (!empty($dataPermissionsJSON)) {
                         $dataPermissionArr = json_decode($dataPermissionsJSON, true);
                         $check = is_Role($dataPermissionArr, $module->name);
+                        return $check;
+                    }
+                    return false;
+                });
+
+                Gate::define($module->name . '.edit', function (User $user) use ($module) {
+                    $dataPermissionsJSON = $user->getGroup->permissions;
+
+                    $dataPermissionArr = [];
+                    if (!empty($dataPermissionsJSON)) {
+                        $dataPermissionArr = json_decode($dataPermissionsJSON, true);
+                        $check = is_Role($dataPermissionArr, $module->name, 'edit');
+                        return $check;
+                    }
+                    return false;
+                });
+                Gate::define($module->name . '.delete', function (User $user) use ($module) {
+                    $dataPermissionsJSON = $user->getGroup->permissions;
+
+                    $dataPermissionArr = [];
+                    if (!empty($dataPermissionsJSON)) {
+                        $dataPermissionArr = json_decode($dataPermissionsJSON, true);
+                        $check = is_Role($dataPermissionArr, $module->name, 'delete');
                         return $check;
                     }
                     return false;
