192437910ebf68004b78a453468330a0192e68dc
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-09-14 11:16:02 +0700

    create page 403

---
+++ 06ProjectAuthorization/resources/views/errors/403.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+
+@section('title', 'Trang không có quyye62n truy cập')
+@section('content')
+    <h1 class="text-center alert alert-danger">Bạn không có quyền truy cập</h1>
+@endsection
